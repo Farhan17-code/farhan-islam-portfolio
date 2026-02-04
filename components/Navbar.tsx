969ea47aa6d38c6be9f62784374061833e5d331f
Farhan17-code <farhanislam11y@gmail.com>
2026-02-04 18:54:37 +0600

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar: React.FC = () => {
     }`}>
       <div className="max-w-7xl mx-auto px-5 md:px-6 flex justify-between items-center">
         <div className="text-xl md:text-2xl font-heading font-black tracking-tighter text-white">
-          FARHAN ISLAM<span className="text-brand-blue">.</span>
+          JAHIRUL ISLAM<span className="text-brand-blue">.</span>
         </div>
         
         <div className="hidden md:flex space-x-8">
